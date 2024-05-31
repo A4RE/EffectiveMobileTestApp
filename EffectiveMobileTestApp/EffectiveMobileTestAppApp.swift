d781ab01e7c0a4823537592b98f02b3a4bf34e19
Andrei <aakoval01@gmail.com>
2024-05-31 10:42:15 +0300

    Initial Commit

---
+++ EffectiveMobileTestApp/EffectiveMobileTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTestAppApp.swift
+//  EffectiveMobileTestApp
+//
+//  Created by Андрей Коваленко on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
